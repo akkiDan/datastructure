df4bee1d69fcc6ab38adaa954c9b8ac860af94c0
akki <akdandwate94@gmail.com>
2022-06-15 08:46:47 +0530

    BubbleSort

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,18 +1,21 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
 class BubbleSort {
     public static void main(String[] args) {
         int[] arr = new int[]{2,7,3,4,6,21,44};
-        for(int i = 0;iarr.length;i++) {
-            for(int j=1;jarr.length-1;j++) {
-                if(arr[j-1]  arr[j] ) {
+        for(int i = 0;i<arr.length;i++) {
+            for(int j=0;j<arr.length-i-1;j++) {
+                if(arr[j+1] < arr[j] ) {
                     int temp = arr[j];
-                    arr[j] = arr[j-1];
-                    arr[j-1] = temp;
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
             }
         }
         
-        for(int i=0;iarr.length;i++) {
-            System.out.println( +arr[i]);
+        for(int i=0;i<arr.length;i++) {
+            System.out.println(" "+arr[i]);
         }
     }
 }
